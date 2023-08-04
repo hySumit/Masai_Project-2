217dc9b893db58230746120ac0d86174bcf48f92
Sumit Kumar <112721086+hySumit@users.noreply.github.com>
2023-08-04 19:07:06 +0530

    api_Weather.py

--- api_Weather.py
+++ api_Weather.py
@@ -1,24 +1,32 @@
-import requests 
-
-city_input = input("Enter Your City Name: ")
+import requests
 
 api_key = "2e80c3da5cae1a7b31a2d88f5169de50"
 
-def get_weather(api_key,city_input):
+def get_weather(api_key, city_input):
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city_input}&appid={api_key}"
 
-    url_response = requests.get(url).json()
+    try:
+        url_response = requests.get(url).json()
 
+        if url_response.get("cod") == 200: # checks the if the response "cod" with value of 200 # https 
+            temperature = url_response["main"]["temp"]
+            temp_celsius = temperature - 273.15 # converts to c
+            temp = format(temp_celsius, ".2f")
+            print("The temperature in", city_input, "is", str(temp) + "°C")
+            return True
+        else:
+            print("Error: Invalid city name or failed in data fetching.")
+            return False
+    except requests.exceptions.RequestException as error:
+        print("Error: An error occurred while making the request.")
+        print(error)
+        return False
 
-    temperature = url_response["main"]["temp"]
-    
-    temp_old = (temperature )-273.15 # coverts to C
-    temp = format(temp_old,".2f") 
-    
-    print("The temperature in",city_input,"is",str(temp)+"°C")
-    
-
-get_weather(api_key, city_input)
+while True: # extra from my side if user puts a wrong city name then again it will ask you to enter your city name
+    city_input = input("Enter Your City Name: ")
+    getweather = get_weather(api_key, city_input)
+    if getweather == True:
+        break
 
 # Hi My name is Sumit i am cap 04 batch. and my project is -  Using API to fetch the weather ☁️🌡️ of the city 
 
